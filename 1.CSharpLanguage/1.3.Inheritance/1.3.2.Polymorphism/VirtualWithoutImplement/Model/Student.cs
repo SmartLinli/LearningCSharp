cf434d23ec9a3cea45e48db02bd7c42df2a533b9
SmartLinli <linli@fjtcm.edu.cn>
2021-04-19 23:23:58 +0800

    update

--- 1.CSharpLanguage/1.3.Inheritance/1.3.2.Polymorphism/VirtualWithoutImplement/Model/Student.cs
+++ 1.CSharpLanguage/1.3.Inheritance/1.3.2.Polymorphism/VirtualWithoutImplement/Model/Student.cs
@@ -16,7 +16,7 @@ namespace SmartLin.LearningCSharp.Inheritance
         /// </summary>
         public virtual void Register()                                  //若各派生类中对该方法的实现完全不同，难以复用代码，则基类的虚方法可不提供具体实现；
         {
-            throw new NotImplementedException();                        //为约束各派生类必须实现该虚方法，可在虚方法中直接抛出异常；一旦某派生类由于开发人员疏忽等情况导致未重写该虚方法，将直接继承该虚方法并随即出错；
+            throw new NotImplementedException();                        //为约定各派生类必须实现该虚方法，可在虚方法中直接抛出异常；一旦某派生类由于开发人员疏忽等情况导致未重写该虚方法，将直接继承该虚方法并随即出错；
         }                                                   
         /// <summary>
         /// 构造函数；
--- 1.CSharpLanguage/1.3.Inheritance/1.3.3.Interface/InterfaceOriented/Program.cs
+++ 1.CSharpLanguage/1.3.Inheritance/1.3.3.Interface/InterfaceOriented/Program.cs
@@ -8,13 +8,13 @@ namespace SmartLin.LearningCSharp.Inheritance
         {
             Faculty mrLin = new Faculty("2004034", "林立");
             Faculty msYang = new Faculty("2002010", "杨雪梅");
-			Faculty mrHuang = new Faculty("2011044", "黄至辉");									
+            Faculty mrHuang = new Faculty("2011044", "黄至辉");
             Student girl = new Student("2190757001", "李四");
             IEvaluate[] audiences = new IEvaluate[] { msYang, mrHuang, girl };          //对象的类型为接口（数组）；
             OpenLesson oop = new OpenLesson()
             {
                 Lecturer = mrLin,
-                Audiences = audiences                           
+                Audiences = audiences
             };
             oop.Evaluate();
             Read();
