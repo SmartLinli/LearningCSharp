3c70455a0b62d00c55cd8dfe5eb8dd63ed56d6e9
Linli <linli@fjtcm.edu.cn>
2021-05-26 15:10:55 +0800

    update

--- 2.WindowsForm/2.1.FormAndControl/History/Examples/100_CSharpLanguage/102_ObjectAndClass/01_Field/ClassAsDataMember/Class.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace SmartLin.LearningCSharp.ClassAndObject
-{
-    public class Class
-    {
-        public int Year;
-        public Major Major;                         //类的数据成员可以是其它类的对象；
-    }
-}
