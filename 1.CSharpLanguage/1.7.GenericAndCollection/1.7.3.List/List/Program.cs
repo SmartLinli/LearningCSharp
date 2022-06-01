6efeb39e36ad1c02f7633cfbc5d9ea387c7d0903
SmartLinli <linli@fjtcm.edu.cn>
2022-06-01 13:30:25 +0800

    update

--- 1.CSharpLanguage/1.7.GenericAndCollection/1.7.3.List/List/Program.cs
+++ 1.CSharpLanguage/1.7.GenericAndCollection/1.7.3.List/List/Program.cs
@@ -27,6 +27,7 @@ namespace SmartLin.LearningCSharp.GenericTypeAndCollection
             WriteLine($"当前个数：{students.Count}。");
             students.TrimExcess();                                                                      //删除额外容量；
             WriteLine($"当前容量：{students.Capacity}。");
+            
             students.Add                                                                                //添加元素；
                 (new Student("3210707011", "陈伟平", Gender.MALE, new DateTime(2003, 6, 1)));
             if (!students.Contains(studentNo1))                                                         //判断是否包含指定元素；
@@ -47,6 +48,7 @@ namespace SmartLin.LearningCSharp.GenericTypeAndCollection
                     });
             students.Insert                                                                             //在指定位置插入元素；
                 (14, new Student("3210707015", "官祥杰", Gender.MALE, new DateTime(2002, 11, 29)));
+            
             /*
             students.Remove                                                                             //查找指定元素并删除；
                 (new Student("3210707001", null, 0));                                                   //查找时将调用equals方法进行匹配；若未重写equals方法，将调用object类的equals方法；
@@ -54,15 +56,25 @@ namespace SmartLin.LearningCSharp.GenericTypeAndCollection
                 (s => s.BirthDate.Value.Year >= 2002);                                                  //条件通过委托指定；
             students.Clear();                                                                           //清除所有元素；
             */
+            
+            string studentNumber = "3210707001";
+            if (students.Exists(s => s.Number == studentNumber))                                        //判断满足条件的元素是否存在；
+            {
+                Student student = students.Find(s => s.Number == studentNumber);                        //返回满足条件的第一项元素；
+                WriteLine($"查得学号为{studentNumber}的学生{student.Name}。");
+            }
+            else
+            {
+                WriteLine($"该班不存在学号为{studentNumber}的学生。");
+            }
+            
             WriteLine("查找2003年之前出生的学生：");
             students.FindAll                                                                            //查找满足条件的所有元素；
-                (s => s.BirthDate.Value.Year < 2003)                                                    //若调用Find方法，则只返回满足条件的第一项元素；
+                (s => s.BirthDate.Value.Year < 2003)                                                    
                 .ForEach                                                                                //迭代每个元素；
                 (s => WriteLine                                                                         //并通过委托执行指定的操作；
                         ($"学号：{s.Number,-15}姓名：{s.Name,-5}\t生日：{s.BirthDate.Value.ToLongDateString()}"));
-            string studentNumber = "3210707100";
-            WriteLine                                                                                   //满足条件的元素是否存在；
-                ($"该班{(students.Exists(s => s.Number == studentNumber) ? "" : "不")}存在学号为{studentNumber}的学生。");
+            
             WriteLine("排序结果：");
             students.Sort();                                                                            //元素必须实现IComparable接口，方可调用无参的Sort方法；
             students.Sort((s, s2) => s.Name.CompareTo(s2.Name));                                        //通过委托指定排序选项；
